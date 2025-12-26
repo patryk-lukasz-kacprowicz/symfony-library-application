ad07957be0ba9c7bcabc1ffcf0127889abbf2bfe
Patryk Kacprowicz <patryk.l.kacprowicz@gmail.com>
2025-12-26 20:01:57 +0100

    ref: add DDD to Book entity

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -19,102 +19,72 @@ class Book
 
     #[ORM\Column]
     #[Groups(['book:read', 'review:read'])]
-    private ?bool $visible = null;
+    private ?bool $visible;
 
     #[ORM\Column(length: 255)]
     #[Groups(['book:read', 'review:read'])]
-    private ?string $name = null;
+    private ?string $name;
 
     #[ORM\Column]
     #[Groups(['book:read', 'review:read'])]
-    private ?int $amount = null;
+    private ?int $amount;
 
     #[ORM\Column]
     #[Groups(['book:read', 'review:read'])]
-    private ?float $price = null;
+    private ?float $price;
 
     #[ORM\Column(length: 255)]
     #[Groups(['book:read', 'review:read'])]
-    private ?string $currency = null;
+    private ?string $currency;
 
     #[ORM\ManyToOne(targetEntity: Author::class, inversedBy: 'books')]
     #[ORM\JoinColumn(nullable: false)]
     #[Groups(['book:read', 'review:read'])]
-    private ?Author $author = null;
+    private ?Author $author;
 
     #[ORM\OneToMany(targetEntity: Review::class, mappedBy: 'book', cascade: ['persist', 'remove'])]
     #[Groups(['book:read'])]
     private Collection $reviews;
 
-    public function __construct() {
+    public function __construct(
+        Author $author,
+        string $name,
+        int $amount,
+        float $price,
+        string $currency,
+        bool $visible = false
+    ) {
         $this->reviews = new ArrayCollection();
-    }
 
-    public function getId(): ?int {
-        return $this->id;
-    }
-
-    public function isVisible(): ?bool {
-        return $this->visible;
-    }
-
-    public function setVisible(bool $visible): static {
-        $this->visible = $visible;
-
-        return $this;
-    }
-
-    public function getName(): ?string {
-        return $this->name;
-    }
-
-    public function setName(string $name): static {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getAmount(): ?int {
-        return $this->amount;
-    }
-
-    public function setAmount(int $amount): static {
-        $this->amount = $amount;
-
-        return $this;
-    }
-
-    public function getPrice(): ?float {
-        return $this->price;
-    }
-
-    public function setPrice(float $price): static {
-        $this->price = $price;
-
-        return $this;
-    }
-
-    public function getCurrency(): ?string {
-        return $this->currency;
-    }
-
-    public function setCurrency(string $currency): static {
-        $this->currency = $currency;
-
-        return $this;
-    }
-
-    public function getAuthor(): ?Author {
-        return $this->author;
-    }
-
-    public function setAuthor(?Author $author): static {
         $this->author = $author;
-
-        return $this;
+        $this->name = $name;
+        $this->amount = $amount;
+        $this->price = $price;
+        $this->currency = $currency;
+        $this->visible = $visible;
     }
 
-    public function getReviews(): Collection {
-        return $this->reviews;
+    public function update(
+        Author $newAuthor,
+        string $newName,
+        int $newAmount,
+        float $newPrice,
+        string $newCurrency,
+        bool $newVisible = false
+    ): void {
+        $this->author = $newAuthor;
+        $this->name = $newName;
+        $this->amount = $newAmount;
+        $this->price = $newPrice;
+        $this->currency = $newCurrency;
+        $this->visible = $newVisible;
     }
+
+    public function getId(): ?int { return $this->id; }
+    public function getName(): ?string { return $this->name; }
+    public function getAmount(): ?int { return $this->amount; }
+    public function getPrice(): ?float { return $this->price; }
+    public function getCurrency(): ?string { return $this->currency; }
+    public function getAuthor(): ?Author { return $this->author; }
+    public function getReviews(): Collection { return $this->reviews; }
 }
