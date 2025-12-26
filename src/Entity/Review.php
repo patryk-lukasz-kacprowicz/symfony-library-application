193f9c4003e21fb96e6838a62ad9f8e5173a03c1
Patryk Kacprowicz <patryk.l.kacprowicz@gmail.com>
2025-12-26 19:57:12 +0100

    ref: add DDD to Review entity

--- src/Entity/Review.php
+++ src/Entity/Review.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ReviewRepository;
 use Doctrine\ORM\Mapping as ORM;
+use http\Exception\InvalidArgumentException;
 use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: ReviewRepository::class)]
@@ -17,48 +18,47 @@ class Review
 
     #[ORM\Column(type: 'float')]
     #[Groups(['review:read', 'book:read'])]
-    private ?int $score = null;
+    private ?int $score;
 
     #[ORM\Column(type: 'text')]
     #[Groups(['review:read', 'book:read'])]
-    private ?string $message = null;
+    private ?string $message;
 
     #[ORM\ManyToOne(targetEntity: Book::class, inversedBy: 'reviews')]
     #[ORM\JoinColumn(nullable: false)]
     #[Groups(['review:read'])]
     private ?Book $book;
 
-    public function getId(): ?int {
-        return $this->id;
-    }
+    public function __construct(
+        Book $book,
+        float $score,
+        string $message
+    ) {
+        $this->validateScore($score);
 
-    public function getScore(): ?int {
-        return $this->score;
-    }
-
-    public function setScore(?int $score): static {
-        $this->score = $score;
-
-        return $this;
-    }
-
-    public function getMessage(): ?string {
-        return $this->message;
-    }
-
-    public function setMessage(?string $message): static {
-        $this->message = $message;
-
-        return $this;
-    }
-
-    public function getBook(): ?Book {
-        return $this->book;
-    }
-
-    public function setBook(?Book $book): static {
         $this->book = $book;
-
-        return $this;
+        $this->score = $score;
+        $this->message = $message;
     }
+
+    public function update(
+        float $newScore,
+        string $newMessage,
+    ): void {
+        $this->validateScore($newScore);
+
+        $this->score = $newScore;
+        $this->message = $newMessage;
+    }
+
+    public function validateScore(float $score): void {
+        if ($score < 0 || $score > 10) {
+            throw new InvalidArgumentException('Score must be between 0 and 10');
+        }
+    }
+
+    public function getId(): ?int { return $this->id; }
+    public function getScore(): ?int { return $this->score; }
+    public function getMessage(): ?string { return $this->message; }
+    public function getBook(): ?Book { return $this->book; }
 }
